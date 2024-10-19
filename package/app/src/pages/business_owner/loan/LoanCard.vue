6a35d490ac19e32d35ababd47318072dc3d4e210
atxing <atx.ang1999@gmail.com>
2024-10-19 18:13:12 +0800

    business owner view loan info dialog

--- package/app/src/pages/business_owner/loan/LoanCard.vue
+++ package/app/src/pages/business_owner/loan/LoanCard.vue
@@ -1,27 +1,47 @@
 <script setup lang="ts">
-import { PrimaryButton } from 'src/components';
+import { Dialog } from 'quasar';
+import { IconButton, PrimaryButton } from 'src/components';
 import { PropType } from 'vue';
 import { Loan } from '~libs/entities';
+import LoanDialog from './dialog/LoanDialog.vue';
 
-defineProps({
+const props = defineProps({
   loan: {
     type: Object as PropType<Loan>,
     required: true,
   },
+  index: {
+    type: Number,
+    required: true,
+  },
 });
+
+function viewLoan() {
+  Dialog.create({
+    component: LoanDialog,
+    componentProps: {
+      loan: props.loan,
+    },
+  });
+}
 </script>
 
 <template>
   <q-card square>
     <q-img
-      src="/images/loan-01.jpg"
+      :src="`/images/loan-0${index % 2 === 0 ? 1 : 2}.jpg`"
       :img-style="{
         filter: 'opacity(90%)',
       }"
     >
+      <div style="position: absolute; right: 0px; background: none">
+        <IconButton icon="info" @click="viewLoan" color="primary" size="md" />
+      </div>
       <div class="absolute-bottom text-subtitle2">
-        <div class="q-gutter-y-md">
-          <div class="text-black text-h5 text-bold">{{ loan.name }}</div>
+        <div class="q-gutter-y-sm">
+          <div class="text-black text-h6 text-bold">
+            {{ loan.name }}
+          </div>
           <PrimaryButton label="Apply" />
         </div>
       </div>
@@ -34,7 +54,8 @@ defineProps({
 </template>
 
 <style lang="sass" scoped>
-.q-img__content > div
+.q-img__content > div.absolute-bottom
   background: rgb(255,255,255,0.6)
   box-shadow: 0 0 5px #ccc
+  padding: 10px
 </style>
--- package/app/src/pages/business_owner/loan/dialog/LoanDialog.vue
+++ package/app/src/pages/business_owner/loan/dialog/LoanDialog.vue
@@ -1,16 +1,13 @@
 <script setup lang="ts">
 import { capitalCase } from 'change-case';
 import dayjs from 'dayjs';
-import { Api } from 'src/api';
 import {
   AppForm,
   DateRangePicker,
   GlobalDialog,
-  PrimaryButton,
   TextInput,
 } from 'src/components';
 import DropdownInput from 'src/components/form/DropdownInput.vue';
-import { useDialog, useLoading } from 'src/composable';
 import { PropType, ref } from 'vue';
 import { LoanDto } from '~api/loan/loan.dto';
 import { Loan } from '~libs/entities';
@@ -22,9 +19,6 @@ const props = defineProps({
     default: undefined,
   },
 });
-
-const { dialogRef, emitData } = useDialog();
-const { loading, toast } = useLoading();
 
 const state = ref<LoanDto>(
   props.loan || {
@@ -47,51 +41,25 @@ function updateDateRange(v: Date[]) {
   state.value.start_at = dayjs(v[0]).startOf('day').toDate();
   state.value.end_at = dayjs(v[1]).endOf('day').toDate();
 }
-
-function submit() {
-  toast(
-    async () => {
-      if (props.loan) {
-        await Api.Loan.updateLoan(state.value);
-        emitData();
-      } else {
-        await Api.Loan.createLoan(state.value);
-        emitData();
-      }
-    },
-    {
-      isLoading: loading,
-      message: props.loan ? 'Updating loan...' : 'Creating loan...',
-      successMessage: props.loan
-        ? 'Updated loan successfully'
-        : 'Created loan successfully',
-    }
-  );
-}
 </script>
 
 <template>
-  <GlobalDialog
-    ref="dialogRef"
-    :title="loan ? 'Update Loan' : 'Create Loan'"
-    padding="md"
-    position="right"
-    @submit="submit"
-  >
+  <GlobalDialog ref="dialogRef" title="Loan Info" padding="md" position="right">
     <div class="q-gutter-y-md">
-      <TextInput v-model="state.name" title="Name" required />
+      <TextInput v-model="state.name" title="Name" disable />
 
-      <AppForm title="Date" required>
+      <AppForm title="Date">
         <DateRangePicker
           :model-value="[state.start_at, state.end_at]"
           title="Date"
           class="full-width"
           :time-picker="false"
           @update:model-value="updateDateRange"
+          disable
         />
       </AppForm>
 
-      <AppForm title="Loan Amount" required>
+      <AppForm title="Loan Amount">
         <div class="row items-center justify-evenly no-wrap">
           <TextInput
             type="number"
@@ -102,6 +70,7 @@ function submit() {
               min: 1,
             }"
             class="full-width"
+            disable
           />
           <div class="q-px-sm">~</div>
           <TextInput
@@ -113,6 +82,7 @@ function submit() {
             }"
             prefix="MYR"
             class="full-width"
+            disable
           />
         </div>
       </AppForm>
@@ -121,10 +91,10 @@ function submit() {
         type="number"
         v-model="state.min_operation_year"
         title="Min Operation Year"
-        required
         :number="{
           min: 1,
         }"
+        disable
       />
 
       <DropdownInput
@@ -136,38 +106,31 @@ function submit() {
           }))
         "
         title="Valid Company Type"
-        required
+        disable
       />
 
       <TextInput
         v-model="state.min_annual_sales"
         title="Min Annual Sales"
-        required
         :number="{
           decimal: 2,
           min: 1,
         }"
+        disable
       />
 
-      <AppForm title="Is Malaysia Company" required>
-        <q-toggle
-          v-model="state.is_malaysia_company"
-          title="Is Malaysia Company"
-        />
+      <AppForm title="Is Malaysia Company">
+        <div>{{ state.is_malaysia_company ? 'Yes' : 'No' }}</div>
       </AppForm>
 
       <TextInput
         v-model="state.instalment_tenure_year"
         title="Instalment Tenure Year"
-        required
         :number="{
           min: 1,
         }"
+        disable
       />
     </div>
-
-    <template #actions>
-      <PrimaryButton label="Submit" type="submit" />
-    </template>
   </GlobalDialog>
 </template>
